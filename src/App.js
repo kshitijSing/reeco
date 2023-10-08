9188cb54492d392d0793bef69e32b98abc1bc5f0
beUrself <44526652+beUrself@users.noreply.github.com>
2023-10-08 16:47:42 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from './components/navbar/navbar';
+import OrderPage from './orders';
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar />
+    <OrderPage />
+    </div>
+  );
+}
+
+export default App;
